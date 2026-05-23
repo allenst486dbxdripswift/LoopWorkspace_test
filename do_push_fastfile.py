6ac310db60a24113e97a29dad9689e87e14fb143
Allen <allen@example.com>
2026-05-23 23:24:35 +0900

    Fix CI: remove invalid set_keychain_settings action

--- do_push_fastfile.py
+++ do_push_fastfile.py
@@ -10,7 +10,7 @@ def run_cmd(cmd):
 
 def main():
     run_cmd("git add -A")
-    run_cmd('git commit -m "Fix CI: remove deprecated xcversion and use runner Xcode"')
+    run_cmd('git commit -m "Fix CI: remove invalid set_keychain_settings action"')
     run_cmd("git push origin caresens_integration")
 
 if __name__ == "__main__":
